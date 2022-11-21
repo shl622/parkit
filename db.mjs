2ba89a33af22f3be843ad75a79f1dc4bc2eb3815
shl622 <shl622@nyu.edu>
2022-11-21 18:00:49 -0500

    login and signup with basic homepage ui added

--- db.mjs
+++ db.mjs
@@ -4,6 +4,7 @@ dotenv.config();
 
 const UserSchema = new Schema({
   username: {type: String, required: true},
+  email : {type:String, required: true, unique: true},
   password: {type: String, required: true, unique: true},
 })
 
